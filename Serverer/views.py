ea17839bcf63f6feeebc58d01a9d3de27ab2718a
dmhpsi <thungthudh@gmail.com>
2018-02-03 00:31:11 +0700

    Change: Response content of getinfo method Change: mp3 resource files tag

--- Serverer/views.py
+++ Serverer/views.py
@@ -4,9 +4,11 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
 import os
-
+from tinytag import TinyTag
 
 # Create your views here.
+from MusicServer.settings import BASE_DIR
+
 
 def home(request):
     return render(request, 'Home.html')
@@ -15,8 +17,11 @@ def home(request):
 def getinfo(request):
     x = {}
     stt = 0
-    for root, dirs, files in os.walk('./Serverer/Resources/Music', followlinks=True):
+    for root, dirs, files in os.walk(BASE_DIR + '\Serverer\Resources\Music'):
         for f in files:
             stt += 1
-            x[str(stt)] = f
+            tag = TinyTag.get(root + '/' + f)
+            duration = int(tag.duration)
+            info = {'name': tag.title, 'artist': tag.artist, 'duration': '{:02d}:{:02d}'.format(duration / 60, duration % 60)}
+            x[str(stt)] = info
     return JsonResponse(x)
